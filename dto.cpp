835304e87bad7e3052e483669d227e172241ad87
Merna_Gramoun <524909@student.fontys.nl>
2024-10-09 13:45:47 +0200

    Co-authored-by: Yordanov,Aleksandar A.D. <a.yordanov@student.fontys.nl>

---
+++ dto.cpp
@@ -0,0 +1,10 @@
+#include <string>
+class Dto{
+    public:
+       std::string DataEntityName; //"N105_StartD
+        bool Payload;
+        std::string PayloadModelName; //"N105_StartD",
+        std::string PayloadModelVersion;//:"";
+   std::string Timestamp; //:1728465970669;
+   std::string version; //:"1.0.0"
+};
