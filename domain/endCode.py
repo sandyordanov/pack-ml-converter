701bbd4eb3561610aa511266645861f6dc8fa864
Aga <521032@student.fontys.nl>
2024-11-22 09:47:19 +0100

    Added all EndCodes

--- domain/endCode.py
+++ domain/endCode.py
@@ -1,12 +1,7 @@
 from enum import Enum
-
-<<<<<<<< HEAD:domain/endCode.py
-class endCode(Enum):
+class EndCode(Enum):
     Not_Active = 0
     Normal_End =1
-========
-class EndCode(Enum):
->>>>>>>> 33f2dc3640fc0aa9d9e22e617e75a8264b87955c:domain/EndCode.py
     Receive_Tasklist_TimeOut =2
     Product_Not_Valid=3
     Operation_Cancelled = 4
